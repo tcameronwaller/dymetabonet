7494bfcaa1dd89306dcbeefd73a4eb52e9d402bc
tcameronwaller <cameron.waller@biochem.utah.edu>
2016-11-04 22:21:00 -0600

    some changes to script

--- interface/script.js
+++ interface/script.js
@@ -1,1 +1,16 @@
+/**
+ * Declare a class to contain properties and methods of the network view.
+ */
+class NetworkView {
+    constructor(data) {
+        this.data = data;
+    };
+};
+
+
+/**
+ * Use an immediately-invoked function expression (IIFE) to establish scope in a convenient container.
+ * An alternative style would be to declare the function and subsequently call it.
+ */
+//(function () {})();
 
