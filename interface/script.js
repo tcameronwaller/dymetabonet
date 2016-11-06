23905aa8499848a7096d6f27d1a44a659ea1a443
tcameronwaller <cameron.waller@biochem.utah.edu>
2016-11-05 23:19:23 -0600

    developed basic architecture

--- interface/script.js
+++ interface/script.js
@@ -2,10 +2,47 @@
  * Declare a class to contain properties and methods of the network view.
  */
 class NetworkView {
+
     constructor(data) {
-        this.data = data;
-    };
-};
+        // Declare variable self to store original instance of the object.
+        var self = this;
+        self.data = data;
+
+        self.initialize();
+        self.update();
+    }
+
+    initialize() {
+
+        var self = this;
+
+        // TODO: Define appropriate margins for neat layout.
+
+        // Select element for network view from DOM.
+        self.divNetwork = d3.select("#network");
+
+        // Determine element dimensions and position.
+        var bounds = divNetwork.node().getBoundingClientRect();
+        self.width = bounds.width * 0.66;
+        self.height = bounds.height;
+
+        // Create SVG element.
+        self.svgNetwork = self.divNetwork.append("svg")
+            .attr("width", self.width)
+            .attr("height", self.height);
+    }
+
+    update() {
+
+        var self = this;
+
+    }
+}
+
+
+// Use element dimensions and position to scale SVG element according to window size.
+// var divNetwork = d3.select(#network);
+// var bounds = divNetwork.node().getBoundingClientRect();
 
 
 /**
